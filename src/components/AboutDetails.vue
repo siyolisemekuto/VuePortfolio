5de936b29a654557d159b290d4ec679446fb0e95
unknown <siyozamekuto2.0@gmail.com>
2022-07-21 16:32:39 +0200

    needs more styling

--- src/components/AboutDetails.vue
+++ src/components/AboutDetails.vue
@@ -2,7 +2,7 @@
         <div id="about">
             <h1>About</h1>
             <div class="row" id="about-info">  
-              <div class="col-md-6 ">
+              <div class="col-md-6 info ">
                 <p class="lh-base">I am a lifelong learner with extensive experience in communication. I enjoy working individually but value teamwork. I have been described as pleasant to be around and enthusiastic. I want to use the web development skills I am learning for business and community development.</p>
                 <p class="lh-base">I enjoy reading political fiction, watching movies and theatre productions.</p>
                 <p class="lh-base"> I looked into coding when I was at a cross-roads in my life. After university I realised I am motivated by creating things and standing apart from the crowd. Taking a coding course satisfied those yearnings in me. From the get go at the Life Choices Bootcamp, I have been able to challenge and express myself in a way that is relevant.</p>
@@ -46,14 +46,20 @@ export default{
 </script>
 
 <style scoped>
+
+#siyolise{
+object-fit:cover;
+display: flex;
+align-items: center;
+justify-content: center;
+}
 #about-info{
   display: flex;
   justify-content: center;
 }
-#siyolise{
-object-fit:cover;
-object-position: center;
-}
+
+
+
 /* .active{
   filter:grayscale(0%)
 } */
